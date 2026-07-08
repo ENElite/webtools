2259fe559fa9a32add2e33d0207562c04339feb0
enelite <enelite@qq.com>
2026-07-08 17:03:40 +0000

    fix(webtools/components): 修复工具页面布局滚动异常的问题

--- packages/webtools/src/components/ToolLayout.tsx
+++ packages/webtools/src/components/ToolLayout.tsx
@@ -10,9 +10,9 @@ interface ToolLayoutProps {
 
 export function ToolLayout({ title, description, children }: ToolLayoutProps) {
     return (
-        <div className='h-screen flex flex-col bg-gradient-to-br from-gray-50 to-gray-100'>
+        <div className='min-h-screen flex flex-col bg-gradient-to-br from-gray-50 to-gray-100'>
             <Header />
-            <main className='container mx-auto px-4 py-8 flex-1 min-h-0 flex flex-col'>
+            <main className='container mx-auto px-4 py-8 flex-1 flex flex-col'>
                 {/* 工具头部 */}
                 <section className='mb-8 shrink-0'>
                     <h1 className='text-3xl font-bold mb-2'>{title}</h1>
@@ -20,7 +20,7 @@ export function ToolLayout({ title, description, children }: ToolLayoutProps) {
                 </section>
 
                 {/* 工具内容 */}
-                <section className='flex-1 min-h-0 flex flex-col'>{children}</section>
+                <section className='flex-1 flex flex-col'>{children}</section>
             </main>
         </div>
     );
